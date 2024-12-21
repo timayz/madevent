8af8a9d28f982aa8a9c0d384fe1e4e3489693696
jonathan <jonathan.lapiquonne@gmail.com>
2024-12-21 14:32:40 -0400

    tmp

---
+++ madevent/src/consumer.rs
@@ -0,0 +1,53 @@
+use crate::Event;
+use url::Url;
+use futures::{stream, Stream};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ConsumerError {
+
+    #[error("url: {0}")]
+    Url(#[from] url::ParseError),
+}
+
+pub struct Consumer {
+    path: String,
+    persistent: bool,
+}
+
+impl Consumer {
+
+    pub fn stream(filter: impl Into<String>) -> Result<impl Stream<Item = Event>, ConsumerError> {
+        let filter = Url::parse(&filter.into())?;
+        Ok(stream::iter(vec![]))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn stream() {
+        let reader = Consumer::stream("non-persistent://*/article").unwrap();
+        todo!()
+    }
+
+    #[tokio::test]
+    async fn stream_use_ns() {
+        let reader = Consumer::stream("non-persistent://eu-west-1/article").unwrap();
+        todo!()
+    }
+
+    #[tokio::test]
+    async fn stream_persistent() {
+        let reader = Consumer::stream("persistent://*/article").unwrap();
+        todo!()
+    }
+
+    #[tokio::test]
+    async fn stream_use_ns_persistent() {
+        let reader = Consumer::stream("persistent://eu-west-1/article").unwrap();
+        todo!()
+    }
+}
